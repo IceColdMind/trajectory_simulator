8f8ab4242ca9f274f8f6824dd210ee22add12a7c
IceColdMind <zivosteam@gmail.com>
2016-08-16 14:46:11 +0200

    Update buggy_adding_program.py

--- buggy_adding_program.py
+++ buggy_adding_program.py
@@ -11,15 +11,20 @@ def projectile_propreties(v_0,t,g = 9.8):
   x_max = ((2*v_y)/(v_x *(g/v_x**2)))      #travel distance
   b = v_y/g
   y_max = -0.5*g*(b**2)+v_y*b   #max height
+  
+  t_flight=x_max/v_x
   x_coor=linspace(0,x_max)
   c = x_coor/v_x
   y_coor = (-0.5*g)*c**2+v_y*c
   plt.plot(x_coor,y_coor)
-  return x_max,y_max
+  return x_max,y_max,t_flight
   
-x,y = projectile_propreties(12, 45)
+x,y,t = projectile_propreties(12, 45)
+t= round(t, 2)
 x= round(x, 2)
 y= round(y,2)
 print('The projectile will travel %s meters' %(x))
 print('The projectile will reach a height of %s meters' %(y))
+print('The projectile will spend %s seconds in the air' %(t))
+
 
